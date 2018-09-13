470ef08813ab4fd2d360d80468623864ff093a58
H0neyCake <dimex4@ya.ru>
2018-09-13 17:14:21 +0300

    Initial commit.

---
+++ DpiGame/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace DpiGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.scoreLb = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // scoreLb
+            // 
+            this.scoreLb.AutoSize = true;
+            this.scoreLb.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLb.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreLb.ForeColor = System.Drawing.Color.Tomato;
+            this.scoreLb.Location = new System.Drawing.Point(770, 475);
+            this.scoreLb.Name = "scoreLb";
+            this.scoreLb.Size = new System.Drawing.Size(33, 40);
+            this.scoreLb.TabIndex = 0;
+            this.scoreLb.Text = "0";
+            this.scoreLb.Click += new System.EventHandler(this.scoreLb_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::DpiGame.Resource1.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(815, 524);
+            this.Controls.Add(this.scoreLb);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label scoreLb;
+    }
+}
+
---
+++ DpiGame/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DpiGame
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap HandlerTexure = Resource1.Handler,
+                      TargetTexure = Resource1.target;
+
+        private Point _targetPosition = new Point(300, 300);
+        private Point _direction = Point.Empty;
+        private int _score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true);
+            UpdateStyles();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            timer2.Interval = r.Next(25, 100);
+            _direction.X = r.Next(-1, 2);
+            _direction.Y = r.Next(-1, 2);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            var localPosition = this.PointToClient(Cursor.Position);
+
+            _targetPosition.X += _direction.X * 10;
+            _targetPosition.Y += _direction.Y * 10;
+
+            if(_targetPosition.X < 0 || _targetPosition.X > 500 )
+            {
+                _direction.X *= -1;
+            }
+            if (_targetPosition.Y < 0 || _targetPosition.Y > 500)
+            {
+                _direction.Y *= -1;
+            }
+
+            Point between = new Point(localPosition.X - _targetPosition.X, localPosition.Y - _targetPosition.Y);
+            float distance = (float)Math.Sqrt((between.X * between.X) + (between.Y * between.Y));
+
+            if (distance < 20)
+            {
+                AddScore(1);
+            }
+
+            var handrerRect = new Rectangle(localPosition.X - 50, localPosition.Y - 50, 100, 100);
+            var targetRect = new Rectangle(_targetPosition.X - 50, _targetPosition.Y - 50, 100, 100);
+
+            g.DrawImage(HandlerTexure, handrerRect);
+            g.DrawImage(TargetTexure, targetRect);
+        }
+
+        private void scoreLb_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddScore(int score)
+        {
+            _score += score;
+            scoreLb.Text = _score.ToString();
+        }
+    }
+}
